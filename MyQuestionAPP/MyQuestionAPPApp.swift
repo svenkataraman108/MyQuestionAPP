a2da966cef6b42112e904b47958d1a93f2e4f450
@svenkataraman108 <kpvenkat108@gmail.com>
2024-05-14 14:35:15 -0500

    Initial Commit

---
+++ MyQuestionAPP/MyQuestionAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyQuestionAPPApp.swift
+//  MyQuestionAPP
+//
+//  Created by Scholar on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MyQuestionAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
